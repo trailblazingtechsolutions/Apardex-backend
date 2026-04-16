9a5377a0dd38f58a49d5477d491c45156a59072c
portable <emmanueltemitopedorcas20@gmail.com>
2026-04-16 09:36:36 +0100

    fix: correct import path for HandlebarsAdapter in mailer module

--- src/mailer/mailer.module.ts
+++ src/mailer/mailer.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MailerModule as NestMailerModule } from '@nestjs-modules/mailer';
-import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/adapters/handlebars.adapter';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { join } from 'path';
 import { MailerService } from './mailer.service';
